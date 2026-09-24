8a6fa47e396b8d49930d19966a7aa1bfbb089c4d
Maynard Demmon <maynarddemmon@gmail.com>
2026-09-24 09:54:47 -0700

    Use App as the root of the dependency tree. Move view classes into a view dir.

--- js/tc/all.js
+++
@@ -1,2 +0,0 @@
-/** Provides a dependency target that pulls in all of the tc package. */
-tc.all = true;
--- js/tc/manifest.js
+++ js/tc/manifest.js
@@ -1,12 +1,14 @@
 JS.Packages(file => {
     const TC_ROOT = globalThis.TC_ROOT ?? '',
         COMPONENT_ROOT = TC_ROOT + 'component/',
-        MODEL_ROOT = TC_ROOT + 'model/';
+        MODEL_ROOT = TC_ROOT + 'model/',
+        VIEW_ROOT = TC_ROOT + 'view/';
     
     file(TC_ROOT + '../../lib/myt.min.js').provides('myt.all');
     file(TC_ROOT + 'tc.js').provides('tc').requires('myt.all');
     file(TC_ROOT + 'timeUtil.js').provides('tc.timeUtil').requires('tc');
     
+    // Component //
     file(COMPONENT_ROOT + 'Misc.js').provides(
         'tc.Spacer','tc.WideView','tc.Panel','tc.MiniPanel','tc.LabeledValue'
     ).requires('tc');
@@ -14,6 +16,7 @@ JS.Packages(file => {
     file(COMPONENT_ROOT + 'Btn.js').provides('tc.Btn','tc.AgentBtn','tc.SquareBtn','tc.UnderlineBtn').requires('tc.SimpleAgentMarker');
     file(COMPONENT_ROOT + 'Grid.js').provides('tc.InfiniteGridWrapper').requires('tc.Btn');
     
+    // Model //
     file(MODEL_ROOT + 'Constraints.js').provides('tc.setConstrainedValue').requires('tc');
     file(MODEL_ROOT + 'NumericStatModel.js').provides('tc.NumericStatModel','tc.NotifyingNumericStatModel').requires('tc');
     file(MODEL_ROOT + 'LocationModel.js').provides('tc.LocationModel').requires('tc');
@@ -21,30 +24,28 @@ JS.Packages(file => {
     file(MODEL_ROOT + 'EventModel.js').provides('tc.EventModel').requires(
         'tc.timeUtil','tc.setConstrainedValue','tc.NotifyingNumericStatModel'
     );
-    file(MODEL_ROOT + 'OperationModel.js').provides('tc.OperationModel').requires(
-        'tc.setConstrainedValue'
-    );
+    file(MODEL_ROOT + 'OperationModel.js').provides('tc.OperationModel').requires('tc.setConstrainedValue');
     file(MODEL_ROOT + 'Model.js').provides('tc.Model').requires(
         'tc.LocationModel','tc.AgentModel','tc.EventModel','tc.OperationModel'
     );
     
-    file(TC_ROOT + 'Agents.js').provides('tc.Agents').requires(
+    // View //
+    file(VIEW_ROOT + 'Agents.js').provides('tc.Agents').requires(
         'tc.timeUtil','tc.Panel','tc.InfiniteGridWrapper','tc.SimpleAgentGridMarker'
     );
-    file(TC_ROOT + 'TimelineCompact.js').provides('tc.TimelineCompact').requires(
+    file(VIEW_ROOT + 'TimelineCompact.js').provides('tc.TimelineCompact').requires(
         'tc.timeUtil','tc.Panel','tc.SquareBtn','tc.SimpleAgentMarker'
     );
-    file(TC_ROOT + 'EventDetails.js').provides('tc.EventDetails').requires(
+    file(VIEW_ROOT + 'EventDetails.js').provides('tc.EventDetails').requires(
         'tc.timeUtil','tc.WideView','tc.Panel','tc.MiniPanel','tc.AgentBtn','tc.SimpleAgentMarker'
     );
-    file(TC_ROOT + 'OperationDetails.js').provides('tc.OperationDetails').requires(
+    file(VIEW_ROOT + 'OperationDetails.js').provides('tc.OperationDetails').requires(
         'tc.timeUtil','tc.WideView','tc.Panel','tc.MiniPanel','tc.UnderlineBtn'
     );
+    
+    // Include Everything
     file(TC_ROOT + 'App.js').provides('tc.App').requires(
         'tc.Model','tc.Agents','tc.Spacer','tc.WideView','tc.Panel',
         'tc.TimelineCompact','tc.EventDetails','tc.OperationDetails'
     );
-    
-    // Include Everything
-    file(TC_ROOT + 'all.js').provides('tc.all').requires('tc.App');
 });
